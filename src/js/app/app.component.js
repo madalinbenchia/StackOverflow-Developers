94c7102a279c8c6b2ca83d424419af63de311dbf
madalinbenchia <33199272+madalinbenchia@users.noreply.github.com>
2018-03-26 11:02:19 +0300

    Add files via upload

---
+++ src/js/app/app.component.js
@@ -0,0 +1,5 @@
+'use strict';
+angular.module('try',['developerList']).
+	component('developer',{
+		templateUrl:'developer.html',
+});
---
+++ src/js/app/app.controller.js
@@ -0,0 +1,4 @@
+angular.module('try').controller('appController', function($scope){
+
+
+})
