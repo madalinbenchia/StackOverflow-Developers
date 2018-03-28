8dc19dcdb9c2a29dd024b6b0125917389541a5ff
Madalin <madalinbenchia9716@gmail.com>
2018-03-28 15:12:05 +0300

    Integrates API

--- src/js/app/devPage.controller.js
+++ src/js/app/devPage.controller.js
@@ -2,10 +2,10 @@
 	var app = angular.module('try');
 
     app.controller('devPageController', function ($scope, $routeParams, $location) {
-        var id = $routeParams.id.substr(1);
+        var id = $routeParams.user_id.substr(1);
 
         $scope.developer = $scope.posts.find(function(element) {
-        	return element.id == id;
+        	return element.user_id == id;
         });
 
         if($scope.developer === undefined) 
